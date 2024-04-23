d81b697aa95b20075b244b1bb3d14ff2e3417fa3
Documenter.jl <documenter@juliadocs.github.io>
2024-04-23 18:33:16 +0000

    build based on 92d2ab5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SevenSegmentDisplay","category":"page"},{"location":"#SevenSegmentDisplay","page":"Home","title":"SevenSegmentDisplay","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SevenSegmentDisplay.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SevenSegmentDisplay]","category":"page"}]
+}
